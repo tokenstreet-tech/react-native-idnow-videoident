dad09903dd6ed703c9b5aafdf1198085e3171f9f
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-02-17 10:35:13 +0100

    refactor: default message string

--- src/errors/LinkingError.ts
+++ src/errors/LinkingError.ts
@@ -1,10 +1,9 @@
 import { Platform } from 'react-native';
 
 export class LinkingError extends Error {
-    private static readonly defaultMessage: string =
-        `The package '@tokenstreet/react-native-idnow-videoident' doesn't seem to be linked. Make sure: \n\n${Platform.select(
-            { ios: "- You have run 'pod install'\n", default: '' }
-        )}- You rebuilt the app after installing the package\n` + `- You are not using Expo managed workflow\n`;
+    private static readonly defaultMessage: string = `The package '@tokenstreet/react-native-idnow-videoident' doesn't seem to be linked. Make sure: \n\n${Platform.select(
+        { ios: "- You have run 'pod install'\n", default: '' }
+    )}- You rebuilt the app after installing the package\n- You are not using Expo managed workflow\n`;
 
     public constructor(message: string = LinkingError.defaultMessage) {
         super(message);
