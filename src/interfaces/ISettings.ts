bddf750f87a3d2f0ff0349c3695c3634742b3c6e
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-02-18 14:25:14 +0100

    docs: explanation

--- src/interfaces/ISettings.ts
+++ src/interfaces/ISettings.ts
@@ -1,6 +1,11 @@
 import type { IAndroidSettings } from './androidSettings/IAndroidSettings';
 import type { IIosSettings } from './iosSettings/IIosSettings';
 
+/**
+ * The complete documentation of the interfaces is copied directly from the READMEs of the respective repositories
+ * https://github.com/idnow/de.idnow.ios
+ * https://github.com/idnow/de.idnow.android
+ */
 export interface ISettings extends IIosSettings, Omit<IAndroidSettings, 'connectionType' | 'environment'> {
     /**
      * iOS Docs:
