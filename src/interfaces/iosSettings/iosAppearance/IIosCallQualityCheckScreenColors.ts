35990b0b58becac1d075a556b74ac884abcf8deb
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-02-18 15:40:24 +0100

    fix: color value

--- src/interfaces/iosSettings/iosAppearance/IIosCallQualityCheckScreenColors.ts
+++ src/interfaces/iosSettings/iosAppearance/IIosCallQualityCheckScreenColors.ts
@@ -1,3 +1,5 @@
+import type { ColorValue } from 'react-native';
+
 /**
  * Call quality check screen colors
  */
@@ -6,32 +8,32 @@ export interface IIosCallQualityCheckScreenColors {
      * Optional color that replaces default dark gray for the outer ring indicator on the quality check screen.
      * Default: dark gray
      */
-    cqcOuterRingColor?: string;
+    cqcOuterRingColor?: ColorValue;
 
     /**
      * Optional color that replaces default light gray for the inner ring indicator on the quality check screen.
      * Default: light gray
      */
-    cqcDefaultInnerRingColor?: string;
+    cqcDefaultInnerRingColor?: ColorValue;
 
     /**
      * Optional color that replaces default bright red for the inner ring indicator,
      * in case bad network quality on the quality check screen.
      * Default: bright red
      */
-    cqcPoorQualityInnerColor?: string;
+    cqcPoorQualityInnerColor?: ColorValue;
 
     /**
      * Optional color that replaces default bright orange for the inner ring indicator,
      * in case moderate network quality on the quality check screen.
      * Default: bright orange
      */
-    cqcModerateQualityInnerColor?: string;
+    cqcModerateQualityInnerColor?: ColorValue;
 
     /**
      * Optional color that replaces default strong yellow for the inner ring indicator,
      * in case excellent network quality on the quality check screen.
      * Default: strong yellow (almost green).
      */
-    cqcExcellentQualityInnerColor?: string;
+    cqcExcellentQualityInnerColor?: ColorValue;
 }
--- src/interfaces/iosSettings/iosAppearance/IIosColors.ts
+++ src/interfaces/iosSettings/iosAppearance/IIosColors.ts
@@ -1,3 +1,5 @@
+import type { ColorValue } from 'react-native';
+
 import type { IIosCallQualityCheckScreenColors } from './IIosCallQualityCheckScreenColors';
 import type { IIosStatusBarColor } from './IIosStatusBarColor';
 
@@ -10,54 +12,54 @@ export interface IIosColors extends IIosStatusBarColor, IIosCallQualityCheckScre
      * Default: A nearly black color
      * Recommendation: Should be some kind of dark color that does not collide with white color.
      */
-    defaultTextColor?: string;
+    defaultTextColor?: ColorValue;
 
     /**
      * Optional color, that replaces the default brand color.
      * Default: defaultTextColor Used in headlines, checkboxes, links, alerts etc.
      * Recommendation: Should be a color that does not collide with white color.
      */
-    primaryBrandColor?: string;
+    primaryBrandColor?: ColorValue;
 
     /**
      * Optional color, that replaces the proceed button background color.
      * Default: An orange color
      */
-    proceedButtonBackgroundColor?: string;
+    proceedButtonBackgroundColor?: ColorValue;
 
     /**
      * Optional color, that replaces the proceed button text color.
      * Default value: White color
      */
-    proceedButtonTextColor?: string;
+    proceedButtonTextColor?: ColorValue;
 
     /**
      * Optional color, that replaces the photo ident retake button background color.
      * Default value: defaultTextColor
      */
-    photoIdentRetakeButtonBackgroundColor?: string;
+    photoIdentRetakeButtonBackgroundColor?: ColorValue;
 
     /**
      * Optional color, that replaces the photo ident retake button text color.
      * Default value: proceedButtonTextColor
      */
-    photoIdentRetakeButtonTextColor?: string;
+    photoIdentRetakeButtonTextColor?: ColorValue;
 
     /**
      * Optional color, that replaces the default color of textfield backgrounds and borders
      * Default: defaultTextColor
      */
-    textFieldColor?: string;
+    textFieldColor?: ColorValue;
 
     /**
      * Optional color, that replaces the text color in the result screen, when an identification failed.
      * Default: A red color
      */
-    failureColor?: string;
+    failureColor?: ColorValue;
 
     /**
      * Optional color, that replaces the text color in the result screen, when an identification was successful.
      * Default: A green color
      */
-    successColor?: string;
+    successColor?: ColorValue;
 }
