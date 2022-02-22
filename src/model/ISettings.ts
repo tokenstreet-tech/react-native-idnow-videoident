8cd2654b11101b4eeeed2af01f1608cbf791405c
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-02-22 12:16:40 +0100

    docs: fix

--- src/model/ISettings.ts
+++ src/model/ISettings.ts
@@ -34,8 +34,10 @@ export interface ISettings<TColor extends ColorValue | ProcessedColorValue = Col
      * iOS header file:
      * Optional: The environment that should be used for the identification (DEV, TEST, LIVE, Custom)
      * The default value is `IDnowEnvironmentNotDefined`.
-     * Then the used environment is based on the prefix of the transaction token (DEV -> DEV, TST -> Test, TS1 -> Test1, TS2 -> Test2, TS3 -> Test3, else -> Live).
-     * You can use the special IDnowEnvironmentCustom to define a custom IDnow installation. If this is done, you need to set the apiHost and websocketHost.
+     * Then the used environment is based on the prefix of the transaction token
+     * (DEV -> DEV, TST -> Test, TS1 -> Test1, TS2 -> Test2, TS3 -> Test3, else -> Live).
+     * You can use the special IDnowEnvironmentCustom to define a custom IDnow installation.
+     * If this is done, you need to set the apiHost and websocketHost.
      *
      *
      * iOS README:
