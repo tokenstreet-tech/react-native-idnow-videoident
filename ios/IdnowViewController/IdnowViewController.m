3f53b2ffbea0412c253e1e760ded66d97a434170
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-02-22 14:57:35 +0100

    chore: uncrustify

--- ios/IdnowViewController/IdnowViewController.m
+++ ios/IdnowViewController/IdnowViewController.m
@@ -70,7 +70,7 @@
 	//                                    Video Ident
 	// -----------------------------------------------------------------------------------
 	appearance.shouldShowBlurOverlayLayer = appearanceSettings[@"shouldShowBlurOverlayLayer"];
-    appearance.alphaBlurViewValue = [RCTConvert CGFloat:appearanceSettings[@"alphaBlurViewValue"]];
+	appearance.alphaBlurViewValue = [RCTConvert CGFloat:appearanceSettings[@"alphaBlurViewValue"]];
 	appearance.lightBlurForIdentCodeContainer = appearanceSettings[@"lightBlurForIdentCodeContainer"];
 
 	// -----------------------------------------------------------------------------------
