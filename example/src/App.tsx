a3d4729a0e3e96290317162a8e934dff9137e528
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-02-22 14:20:58 +0100

    feat: improved example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,47 @@
+import type { ISettings } from '@tokenstreet/react-native-idnow-videoident';
 import { IDnowManager } from '@tokenstreet/react-native-idnow-videoident';
 import React, { useCallback, useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+import { getRandomColor } from './util';
+
+const createSettings = (transactionToken: string): ISettings => ({
+    transactionToken,
+    appearance: {
+        colors: {
+            defaultTextColor: getRandomColor(),
+            primaryBrandColor: getRandomColor(),
+            headlineColor: getRandomColor(),
+            linkColor: getRandomColor(),
+            checkIconColor: getRandomColor(),
+            proceedButtonBackgroundColor: getRandomColor(),
+            proceedButtonTextColor: getRandomColor(),
+            photoIdentRetakeButtonBackgroundColor: getRandomColor(),
+            photoIdentRetakeButtonTextColor: getRandomColor(),
+            checkContractFooterButtonBackgroundColor: getRandomColor(),
+            checkContractFooterButtonTextColor: getRandomColor(),
+            textFieldColor: getRandomColor(),
+            textFieldBorderColor: getRandomColor(),
+            failureColor: getRandomColor(),
+            successColor: getRandomColor(),
+            titleBackgroundColor: getRandomColor(),
+            resultScreenHeaderLabelColor: getRandomColor(),
+            backgroundColor: getRandomColor(),
+            cqcOuterRingColor: getRandomColor(),
+            cqcDefaultInnerRingColor: getRandomColor(),
+            cqcPoorQualityInnerColor: getRandomColor(),
+            cqcModerateQualityInnerColor: getRandomColor(),
+            cqcExcellentQualityInnerColor: getRandomColor(),
+        },
+    },
+});
 
 export const App: React.FC = () => {
     const [transactionToken, setTransactionToken] = useState<string>('TST-KJCXN');
     const [videoIdentResponse, setVideoIdentResponse] = useState<string>('');
     const startVideoIdent = useCallback(async () => {
         setVideoIdentResponse('');
-        IDnowManager.startVideoIdent({ transactionToken })
+        IDnowManager.startVideoIdent(createSettings(transactionToken))
             .then((fulfilled) => setVideoIdentResponse(`Resolved:\n${JSON.stringify(fulfilled)}`))
             .catch((rejected) => setVideoIdentResponse(`Rejected:\n${JSON.stringify(rejected)}`));
     }, [transactionToken]);
