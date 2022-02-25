cff506a2f7502f19a26801264600d67e0121f9a5
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-02-25 14:35:57 +0100

    feat: createFullSettings

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,40 +1,40 @@
-// import type { ISettings } from '@tokenstreet/react-native-idnow-videoident';
+import type { ISettings } from '@tokenstreet/react-native-idnow-videoident';
 import { IDnowManager } from '@tokenstreet/react-native-idnow-videoident';
 import React, { useCallback, useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
-// import { getRandomColor } from './util';
+import { getRandomColor } from './util';
 
-// const createFullSettings = (transactionToken: string): ISettings => ({
-//     transactionToken,
-//     appearance: {
-//         colors: {
-//             defaultTextColor: getRandomColor(),
-//             primaryBrandColor: getRandomColor(),
-//             headlineColor: getRandomColor(),
-//             linkColor: getRandomColor(),
-//             checkIconColor: getRandomColor(),
-//             proceedButtonBackgroundColor: getRandomColor(),
-//             proceedButtonTextColor: getRandomColor(),
-//             photoIdentRetakeButtonBackgroundColor: getRandomColor(),
-//             photoIdentRetakeButtonTextColor: getRandomColor(),
-//             checkContractFooterButtonBackgroundColor: getRandomColor(),
-//             checkContractFooterButtonTextColor: getRandomColor(),
-//             textFieldColor: getRandomColor(),
-//             textFieldBorderColor: getRandomColor(),
-//             failureColor: getRandomColor(),
-//             successColor: getRandomColor(),
-//             titleBackgroundColor: getRandomColor(),
-//             resultScreenHeaderLabelColor: getRandomColor(),
-//             backgroundColor: getRandomColor(),
-//             cqcOuterRingColor: getRandomColor(),
-//             cqcDefaultInnerRingColor: getRandomColor(),
-//             cqcPoorQualityInnerColor: getRandomColor(),
-//             cqcModerateQualityInnerColor: getRandomColor(),
-//             cqcExcellentQualityInnerColor: getRandomColor(),
-//         },
-//     },
-// });
+const createFullSettings = (transactionToken: string): ISettings => ({
+    transactionToken,
+    appearance: {
+        colors: {
+            defaultTextColor: getRandomColor(),
+            primaryBrandColor: getRandomColor(),
+            headlineColor: getRandomColor(),
+            linkColor: getRandomColor(),
+            checkIconColor: getRandomColor(),
+            proceedButtonBackgroundColor: getRandomColor(),
+            proceedButtonTextColor: getRandomColor(),
+            photoIdentRetakeButtonBackgroundColor: getRandomColor(),
+            photoIdentRetakeButtonTextColor: getRandomColor(),
+            checkContractFooterButtonBackgroundColor: getRandomColor(),
+            checkContractFooterButtonTextColor: getRandomColor(),
+            textFieldColor: getRandomColor(),
+            textFieldBorderColor: getRandomColor(),
+            failureColor: getRandomColor(),
+            successColor: getRandomColor(),
+            titleBackgroundColor: getRandomColor(),
+            resultScreenHeaderLabelColor: getRandomColor(),
+            backgroundColor: getRandomColor(),
+            cqcOuterRingColor: getRandomColor(),
+            cqcDefaultInnerRingColor: getRandomColor(),
+            cqcPoorQualityInnerColor: getRandomColor(),
+            cqcModerateQualityInnerColor: getRandomColor(),
+            cqcExcellentQualityInnerColor: getRandomColor(),
+        },
+    },
+});
 
 const styles = StyleSheet.create({
     container: {
@@ -49,7 +49,7 @@ export const App: React.FC = () => {
     const [videoIdentResponse, setVideoIdentResponse] = useState<string>('');
     const startVideoIdent = useCallback(() => {
         setVideoIdentResponse('');
-        IDnowManager.startVideoIdent({ transactionToken })
+        IDnowManager.startVideoIdent(createFullSettings(transactionToken))
             .then((fulfilled) => setVideoIdentResponse(`Resolved:\n${JSON.stringify(fulfilled)}`))
             .catch((rejected) => setVideoIdentResponse(`Rejected:\n${JSON.stringify(rejected)}`));
     }, [transactionToken]);
