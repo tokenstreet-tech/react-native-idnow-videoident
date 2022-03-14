f79b59a07a6b28923bdcfe8aaa088ffd87d4f553
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-03-14 17:24:57 +0100

    chore: more strict eslint rules

--- example/metro.config.js
+++ example/metro.config.js
@@ -18,7 +18,9 @@ module.exports = {
      * So we blacklist them at the root, and alias them to the versions in example's node_modules
      */
     resolver: {
-        blacklistRE: blacklist(modules.map((m) => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`))),
+        blacklistRE: blacklist(
+            modules.map((module) => new RegExp(`^${escape(path.join(root, 'node_modules', module))}\\/.*$`))
+        ),
 
         extraNodeModules: modules.reduce((acc, name) => {
             acc[name] = path.join(__dirname, 'node_modules', name);
--- example/src/util.ts
+++ example/src/util.ts
@@ -1,7 +1,7 @@
 export const getRandomColor = (): string => {
     const letters = '0123456789ABCDEF';
     let color = '#';
-    for (let i = 0; i < 6; i += 1) {
+    for (let index = 0; index < 6; index += 1) {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
