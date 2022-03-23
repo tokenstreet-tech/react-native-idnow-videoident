5ae86564a036ea98f764f748834099f8a821bf00
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-03-23 08:50:52 +0100

    feat: new images

--- website/src/components/HomepageFeatures/index.tsx
+++ website/src/components/HomepageFeatures/index.tsx
@@ -1,49 +1,55 @@
 /* eslint-disable react-native/no-raw-text */
-import undrawDocusaurusMountain from '@site/static/img/undraw_docusaurus_mountain.svg';
-import undrawDocusaurusReact from '@site/static/img/undraw_docusaurus_react.svg';
-import undrawDocusaurusTree from '@site/static/img/undraw_docusaurus_tree.svg';
-import React from 'react';
+import configurable from '@site/static/img/configurable.svg';
+import dependabot from '@site/static/img/dependabot.svg';
+import typescript from '@site/static/img/typescript.svg';
+import React, { useMemo } from 'react';
 
 import type { IFeatureItem } from './Feature';
 import { Feature } from './Feature';
 import styles from './index.module.css';
 
-const FeatureList: Array<IFeatureItem> = [
-    {
-        Svg: undrawDocusaurusMountain,
-        description: <>All configuration options of the SDKs are possible from the JavaScript side.</>,
-        title: 'Highly configurable',
-    },
-    {
-        Svg: undrawDocusaurusTree,
-        description: (
-            <>
-                Latest dependencies of the IDnow <a href="https://github.com/idnow/de.idnow.ios">iOS</a> and{' '}
-                <a href="https://github.com/idnow/de.idnow.android">Android</a> SDKs. Secured by Dependabot.
-            </>
-        ),
-        title: 'Always up to date',
-    },
-    {
-        Svg: undrawDocusaurusReact,
-        description: (
-            <>
-                Strong type declarations are included in this package. There isn&apos;t a single <code>any</code>.
-            </>
-        ),
-        title: 'First class TypeScript support',
-    },
-];
+const HomepageFeatures: React.FC = () => {
+    const FeatureList = useMemo<Array<IFeatureItem>>(
+        () => [
+            {
+                Svg: configurable,
+                description: <>All configuration options of the SDKs are possible from the JavaScript side.</>,
+                title: 'Highly configurable',
+            },
+            {
+                Svg: dependabot,
+                description: (
+                    <>
+                        Latest dependencies of the IDnow <a href="https://github.com/idnow/de.idnow.ios">iOS</a> and{' '}
+                        <a href="https://github.com/idnow/de.idnow.android">Android</a> SDKs. Secured by Dependabot.
+                    </>
+                ),
+                title: 'Always up to date',
+            },
+            {
+                Svg: typescript,
+                description: (
+                    <>
+                        Strong type declarations are included in this package. There isn&apos;t a single{' '}
+                        <code>any</code>.
+                    </>
+                ),
+                title: 'First class TypeScript support',
+            },
+        ],
+        []
+    );
 
-const HomepageFeatures: React.FC = () => (
-    <section className={styles.features}>
-        <div className="container">
-            <div className="row">
-                {FeatureList.map((props: IFeatureItem) => (
-                    <Feature key={props.title} {...props} />
-                ))}
+    return (
+        <section className={styles.features}>
+            <div className="container">
+                <div className="row">
+                    {FeatureList.map((props: IFeatureItem) => (
+                        <Feature key={props.title} {...props} />
+                    ))}
+                </div>
             </div>
-        </div>
-    </section>
-);
+        </section>
+    );
+};
 export default HomepageFeatures;
