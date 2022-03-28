3c57ae879cfe1d415c9e6e9e4a62ada57f062003
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-03-28 21:00:15 +0200

    fix: remove docusaurus

--- website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-    require('/Users/danielreichhart/workspace/react-native-idnow-videoident/website/node_modules/infima/dist/css/default/default.css'),
-    require('/Users/danielreichhart/workspace/react-native-idnow-videoident/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-    require('/Users/danielreichhart/workspace/react-native-idnow-videoident/website/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-    require('/Users/danielreichhart/workspace/react-native-idnow-videoident/website/src/css/custom.css'),
-];
