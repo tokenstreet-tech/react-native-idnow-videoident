30fceb36a8bf36aa464ab2dfa4e279762bc79694
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-03-31 11:27:41 +0200

    fix: name

--- example/android/app/src/main/java/com/example/tokenstreetreactnativeidnowvideoident/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
+++ example/android/app/src/main/java/com/example/tokenstreetreactnativeidnowvideoident/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
@@ -45,7 +45,7 @@ public class MainApplicationTurboModuleManagerDelegate extends ReactPackageTurbo
         if (!sIsSoLibraryLoaded) {
             // If you change the name of your application .so file in the Android.mk file,
             // make sure you update the name here as well.
-            SoLoader.loadLibrary("rndiffapp_appmodules");
+            SoLoader.loadLibrary("example_tokenstreetreactnativeidnowvideoident_appmodules");
             sIsSoLibraryLoaded = true;
         }
     }
