d3f1746c322fdee7ae944b6dcae386ef0ab4795b
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-04-23 18:58:19 +0200

    chore: fix

--- packages/example/babel.config.js
+++ packages/example/babel.config.js
@@ -1,15 +1,3 @@
-const path = require('path');
-const pak = require('../package.json');
-
 module.exports = {
-    plugins: [
-        [
-            'module-resolver',
-            {
-                alias: { [pak.name]: path.join(__dirname, '..', pak.source) },
-                extensions: ['.tsx', '.ts', '.js', '.json'],
-            },
-        ],
-    ],
     presets: ['module:metro-react-native-babel-preset'],
 };
