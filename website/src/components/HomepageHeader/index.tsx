f6a86e5e9f92a6c4489f0bdc62690487b9cf700a
Daniel Reichhart <34721312+reichhartd@users.noreply.github.com>
2022-04-24 11:04:40 +0200

    docs: refactor website (#244)

--- website/src/components/HomepageHeader/index.tsx
+++ website/src/components/HomepageHeader/index.tsx
@@ -14,8 +14,8 @@ export const HomepageHeader: React.FC = () => {
                 <h1 className="hero__title">{siteConfig.title}</h1>
                 <p className="hero__subtitle">{siteConfig.tagline}</p>
                 <div className={styles.buttons}>
-                    <Link className="button button--secondary button--lg" to="/docs/api">
-                        Get started
+                    <Link className="button button--secondary button--lg" to="/docs/getting-started">
+                        Getting started
                     </Link>
                 </div>
             </div>
