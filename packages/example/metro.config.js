99a6fe99056ef27e9142a01d6d97921ad7990e90
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-04-24 20:43:32 +0200

    fix: test

--- packages/example/metro.config.js
+++ packages/example/metro.config.js
@@ -1,9 +1,15 @@
+/* eslint-disable sort-keys */
 /**
  * Metro configuration for React Native
  * https://github.com/facebook/react-native
  *
  * @format
  */
+
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+const { getMetroTools } = require('react-native-monorepo-tools');
+
+const monorepoMetroTools = getMetroTools();
 
 module.exports = {
     transformer: {
@@ -13,5 +19,16 @@ module.exports = {
                 inlineRequires: true,
             },
         }),
+    },
+    // Add additional Yarn workspace package roots to the module map.
+    // This allows importing from all the project's packages.
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    watchFolders: monorepoMetroTools.watchFolders,
+    resolver: {
+        // Ensure we resolve nohoist libraries from this directory.
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        blockList: exclusionList(monorepoMetroTools.blockList),
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        extraNodeModules: monorepoMetroTools.extraNodeModules,
     },
 };
