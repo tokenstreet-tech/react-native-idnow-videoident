2e95d24c5b5e5d36431c149b682640eb83d53c59
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-11-04 20:28:08 +0100

    chore: expoExample

--- exampleExpo/src/App.tsx
+++ exampleExpo/src/App.tsx
@@ -1,36 +1,32 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { IDnowManager } from '@tokenstreet/react-native-idnow-videoident';
+import React, { useCallback, useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
-/* eslint-disable @typescript-eslint/no-unused-vars */
-// @ts-expect-error Import remains to be able to switch between screens more easily
-import { HotModuleReloadScreen } from './screens/HotModuleReloadScreen';
-// @ts-expect-error Same description as above
-import { JestTestingScreen } from './screens/JestTestingScreen';
-// @ts-expect-error Same description as above
-import { MediaQueriesScreen } from './screens/MediaQueriesScreen';
-// @ts-expect-error Same description as above
-import { ReadmeScreen } from './screens/ReadmeScreen';
-// @ts-expect-error Same description as above
-import { RemScreen } from './screens/RemScreen';
-import { SimpleScreen } from './screens/SimpleScreen';
-// @ts-expect-error Same description as above
-import { ThemingScreen } from './screens/theming/ThemingScreen';
-/* eslint-enable @typescript-eslint/no-unused-vars */
+import { createFullSettings } from './createFullSettings';
 
-/**
- * The screens must be rendered individually,
- * because each screen calls EStyleSheet.build() with the respective global variables.
- * Otherwise, the screens would interfere with each other.
- * @constructor
- */
-export const App: React.FC = () => (
-    <SafeAreaView>
-        {/* <HotModuleReloadScreen />*/}
-        {/* <JestTestingScreen />*/}
-        {/* <MediaQueriesScreen />*/}
-        {/* <ReadmeScreen />*/}
-        {/* <RemScreen />*/}
-        <SimpleScreen />
-        {/* <ThemingScreen />*/}
-    </SafeAreaView>
-);
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        flex: 1,
+        justifyContent: 'center',
+    },
+});
+
+export const App: React.FC = () => {
+    const [transactionToken, setTransactionToken] = useState<string>('YOUR_TRANSACTION_TOKEN');
+    const [videoIdentResponse, setVideoIdentResponse] = useState<string>('');
+    const startVideoIdent = useCallback(() => {
+        setVideoIdentResponse('');
+        IDnowManager.startVideoIdent(createFullSettings(transactionToken))
+            .then((fulfilled) => setVideoIdentResponse(`Resolved:\n${JSON.stringify(fulfilled)}`))
+            .catch((rejected) => setVideoIdentResponse(`Rejected:\n${JSON.stringify(rejected)}`));
+    }, [transactionToken]);
+
+    return (
+        <View style={styles.container}>
+            <TextInput onChangeText={setTransactionToken} placeholder="Transaction token" value={transactionToken} />
+            <Button onPress={startVideoIdent} title="Start video ident" />
+            <Text>{videoIdentResponse}</Text>
+        </View>
+    );
+};
--- exampleExpo/src/__mocks__/react-native-extended-stylesheet.ts
+++
@@ -1,7 +0,0 @@
-const EStyleSheet = require('react-native-extended-stylesheet').default;
-
-EStyleSheet.build({
-    $fontColor: '#F5FCFF',
-});
-
-module.exports = EStyleSheet;
