20864cc72bb433a9948ca69ef701afa4aec4c610
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-11-04 21:09:39 +0100

    chore: expo

--- examples/bare/metro.config.js
+++ examples/bare/metro.config.js
@@ -5,7 +5,7 @@ const pak = require('../../package.json');
 
 const root = path.resolve(__dirname, '..');
 
-const modules = Object.keys({...pak.peerDependencies});
+const modules = Object.keys({ ...pak.peerDependencies });
 
 module.exports = {
     projectRoot: __dirname,
