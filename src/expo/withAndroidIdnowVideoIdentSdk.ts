66000384275bec4eba9c4a7dbbf47b788b09a33e
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-11-05 15:31:48 +0100

    chore: expo

--- src/expo/withAndroidIdnowVideoIdentSdk.ts
+++ src/expo/withAndroidIdnowVideoIdentSdk.ts
@@ -1,17 +1,26 @@
 // Add the following to allprojects/repositories in android/build.gradle
 export const applyRepositories = (appBuildGradle: string): string => {
-    const idnowRepositories = `maven {
-        url "https://raw.githubusercontent.com/idnow/de.idnow.android.sdk/master"
-     }
-     maven {
-        url "https://raw.githubusercontent.com/idnow/de.idnow.android/de.idnow.android-5.0.12.1"
-     }`;
+    const idnowRepositories =
+        '        jcenter() {\n' +
+        '            // JCenter is now read-only. Therefore, no new versions are published there any more.\n' +
+        '            // We only fetch the necessary dependencies for IDnow from JCenter to avoid loading old dependencies.\n' +
+        '            content {\n' +
+        '                includeModule("me.relex", "circleindicator")\n' +
+        '                includeModule("com.github.barteksc", "android-pdf-viewer")\n' +
+        '            }\n' +
+        '        }\n' +
+        '        maven() {\n' +
+        '            url "https://raw.githubusercontent.com/idnow/de.idnow.android/master"\n' +
+        '            content {\n' +
+        '                includeModule("de.idnow.sdk", "idnow-android-sdk")\n' +
+        '                includeModule("de.idnow.insights", "idnow-android-insights-sdk")\n' +
+        '            }\n' +
+        '        }';
 
     // Make sure the project does not have the repositories already
     if (!appBuildGradle.includes(idnowRepositories)) {
-        const searchRegex = /allprojects\s{\n.*repositories\s{\n/su;
-        const test = appBuildGradle.replace(searchRegex, (substring) => substring.concat(idnowRepositories));
-        return test;
+        const searchRegex = /allprojects\s\{\n.*repositories\s\{\n/su;
+        return appBuildGradle.replace(searchRegex, (substring) => substring.concat(idnowRepositories));
     }
 
     return appBuildGradle;
