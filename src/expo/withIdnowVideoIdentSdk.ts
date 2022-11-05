d2fdaf174887fedeb3ef239eaa806d91adb75f05
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-11-05 14:48:33 +0100

    chore: expo

--- src/expo/withIdnowVideoIdentSdk.ts
+++ src/expo/withIdnowVideoIdentSdk.ts
@@ -1,50 +1,14 @@
 import type { ConfigPlugin } from '@expo/config-plugins';
-import {
-    createRunOncePlugin,
-    withAndroidManifest,
-    withAppBuildGradle,
-    withMainApplication,
-    withProjectBuildGradle,
-    withSettingsGradle,
-} from '@expo/config-plugins';
+import { createRunOncePlugin, withProjectBuildGradle } from '@expo/config-plugins';
 import type { ExpoConfig } from '@expo/config-types';
 
 // @ts-expect-error The TypeScript compiler doesn't resolve the relative import path
 import pak from '../../../package.json';
 import type pakType from '../../package.json';
-import {
-    applyImplementation,
-    applyManifestConfig,
-    applyPackage,
-    applyPackagingOptionsAndConfigurations,
-    applyRepositories,
-    applySettings,
-} from './withAndroidIdnowVideoIdentSdk';
+import { applyRepositories } from './withAndroidIdnowVideoIdentSdk';
 import { withPodfileUpdate } from './withIosIdnowVideoIdentSdk';
 
 const withIdnowVideoIdentSdk: ConfigPlugin = (expoConfig: ExpoConfig) => {
-    expoConfig = withMainApplication(expoConfig, (config) => {
-        config.modResults.contents = applyPackage(config.modResults.contents);
-        return config;
-    });
-
-    expoConfig = withAndroidManifest(expoConfig, async (config) => {
-        // Modifiers can be async, but try to keep them fast.
-        config.modResults = await applyManifestConfig(config, config.modResults);
-        return config;
-    });
-
-    expoConfig = withSettingsGradle(expoConfig, (config) => {
-        config.modResults.contents = applySettings(config.modResults.contents);
-        return config;
-    });
-
-    expoConfig = withAppBuildGradle(expoConfig, (config) => {
-        config.modResults.contents = applyImplementation(config.modResults.contents);
-        config.modResults.contents = applyPackagingOptionsAndConfigurations(config.modResults.contents);
-        return config;
-    });
-
     expoConfig = withProjectBuildGradle(expoConfig, (config) => {
         config.modResults.contents = applyRepositories(config.modResults.contents);
         return config;
