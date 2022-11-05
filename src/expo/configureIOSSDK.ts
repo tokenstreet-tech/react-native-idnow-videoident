7e70a5c0820d019c238506fd289703b83e605814
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-11-05 11:57:09 +0100

    chore: expo

--- src/expo/configureIOSSDK.ts
+++ src/expo/configureIOSSDK.ts
@@ -36,29 +36,14 @@ export const withPodfileUpdate = (config: ExpoConfig) =>
         'ios',
         async (config) => {
             await editPodfile(config, (podfile) => {
-                // Fix for an error taken from here
-                // https://dev.to/kylefoo/xcode-12-new-build-system-warns-multiple-commands-produce-assets-car-56im
-                // solution (2)
                 podfile = addLines(podfile, ':deterministic_uuids => false', 1, [
+                    '',
                     "plugin 'cocoapods-user-defined-build-types'",
                     'enable_user_defined_build_types!',
                 ]);
-                podfile = addLines(podfile, 'config = use_native_modules!', 1, [
-                    "pod 'IDnowSDK', '5.3.0', build_type: :static_framework",
-                ]);
-
-                // Fix from https://github.com/idnow/de.idnow.ios#cocoapods--xcode-9
-                podfile = addLines(podfile, 'react_native_post_install', 2, [
+                podfile = addLines(podfile, ':app_path => "#{Dir.pwd}/.."\n)', 1, [
                     '',
-                    // 'post_install do |installer|', // this was used before a similar section started being included on the podFile
-                    `    copy_pods_resources_path = "Pods/Target Support Files/Pods-${config.modRequest.projectName}/Pods-${config.modRequest.projectName}-resources.sh"`, // '	string_to_replace = \'--compile "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"\'',
-                    '    string_to_replace = \'if [[ $line != "${PODS_ROOT}*" ]]; then\'', // '	assets_compile_with_app_icon_arguments = \'--compile "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}" --app-icon "${ASSETCATALOG_COMPILER_APPICON_NAME}" --output-partial-info-plist "${BUILD_DIR}/assetcatalog_generated_info.plist"\'',
-                    '    assets_compile_with_app_icon_arguments = \'if [[ $line != "${PODS_ROOT}*" && $line != *"Sample"* ]]; then\'',
-                    '    text = File.read(copy_pods_resources_path)',
-                    '    new_contents = text.gsub(string_to_replace, assets_compile_with_app_icon_arguments)',
-                    '    File.open(copy_pods_resources_path, "w") {|file| file.puts new_contents }',
-                    // 'end', // this was used before a similar section started being included on the podFile
-                    '',
+                    "  pod 'IDnowSDK', '5.3.0', build_type: :static_framework",
                 ]);
                 return podfile;
             });
