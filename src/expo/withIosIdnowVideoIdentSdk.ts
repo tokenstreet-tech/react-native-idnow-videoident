e2515a4680337698e04a37ac2f7b8f0496c74117
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-11-07 01:25:56 +0100

    chore: fix

--- src/expo/withIosIdnowVideoIdentSdk.ts
+++ src/expo/withIosIdnowVideoIdentSdk.ts
@@ -27,35 +27,31 @@ const addLines = (content: string, find: string, offset: number, toAdd: string):
 };
 
 const buildTypeModification =
-    '    $static_library = %w[IDnowSDK Masonry SocketRocket libPhoneNumber-iOS FLAnimatedImage AFNetworking]\n' +
+    '  $static_library = %w[IDnowSDK Masonry SocketRocket libPhoneNumber-iOS FLAnimatedImage AFNetworking]\n' +
     '\n' +
-    '    pre_install do |installer|\n' +
-    '        Pod::Installer::Xcode::TargetValidator.send(\n' +
-    '            :define_method,\n' +
-    '            :verify_no_static_framework_transitive_dependencies\n' +
-    '        ) {}\n' +
-    '        installer.pod_targets.each do |pod|\n' +
-    '            bt = pod.send(:build_type)\n' +
-    '            if $static_library.include?(pod.name)\n' +
-    '                puts "Overriding the build_type to static_framework from static_library for #{pod.name}"\n' +
-    '                def pod.build_type\n' +
-    '                    Pod::BuildType.static_framework\n' +
-    '                end\n' +
-    '            end\n' +
+    '  pre_install do |installer|\n' +
+    '    Pod::Installer::Xcode::TargetValidator.send(:define_method, :verify_no_static_framework_transitive_dependencies) {}\n' +
+    '    installer.pod_targets.each do |pod|\n' +
+    '      bt = pod.send(:build_type)\n' +
+    '      if $static_library.include?(pod.name)\n' +
+    '        puts "Overriding the build_type to static_framework from static_library for #{pod.name}"\n' +
+    '        def pod.build_type\n' +
+    '          Pod::BuildType.static_framework\n' +
     '        end\n' +
-    '        installer.pod_targets.each do |pod|\n' +
-    '            bt = pod.send(:build_type)\n' +
-    '            puts "#{pod.name} (#{bt})"\n' +
-    '            puts "  linkage: #{bt.send(:linkage)} packaging: #{bt.send(:packaging)}"\n' +
-    '        end\n' +
-    '    end\n';
+    '      end\n' +
+    '    end\n' +
+    '    installer.pod_targets.each do |pod|\n' +
+    '      bt = pod.send(:build_type)\n' +
+    '      puts "#{pod.name} (#{bt})"\n' +
+    '      puts "  linkage: #{bt.send(:linkage)} packaging: #{bt.send(:packaging)}"\n' +
+    '    end\n' +
+    '  end\n';
 const appleSiliconFix =
-    '    # https://github.com/expo/expo/issues/15800' +
-    '    post_install do |installer|\n' +
-    '      installer.pods_project.targets.each do |target|\n' +
-    '        target.build_configurations.each do |config|\n' +
-    '          config.build_settings["ONLY_ACTIVE_ARCH"] = "NO"\n' +
-    '        end\n' +
+    '\n' +
+    '    # https://github.com/expo/expo/issues/15800\n' +
+    '    installer.pods_project.targets.each do |target|\n' +
+    '      target.build_configurations.each do |config|\n' +
+    '        config.build_settings["ONLY_ACTIVE_ARCH"] = "NO"\n' +
     '      end\n' +
     '    end\n';
 
