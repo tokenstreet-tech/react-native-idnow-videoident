259c516d4b8047f7a2dead389ab9bfdc0e8a17f6
Daniel Reichhart <34721312+reichhartd@users.noreply.github.com>
2022-11-07 02:19:35 +0100

    Removal of the  CocoaPods plugin 'cocoapods-user-defined-build-types' (#593)
    * feat: draft
    
    * feat: draft
    
    * feat: draft
    
    * feat: draft
    
    * feat: draft
    
    * chore: fix
    
    * chore: fix
    
    * chore: fix
    
    * chore: fix
    
    * chore: fix
    
    * chore: docs
    
    * chore: docs
    
    * chore: docs
    
    * chore: docs

--- src/expo/withIdnowVideoIdentSdk.ts
+++ src/expo/withIdnowVideoIdentSdk.ts
@@ -2,10 +2,9 @@ import type { ConfigPlugin } from '@expo/config-plugins';
 import { createRunOncePlugin } from '@expo/config-plugins';
 import type { ExpoConfig } from '@expo/config-types';
 
+import { typedPak } from './typedPak';
 import { withIdnowRepositories } from './withAndroidIdnowVideoIdentSdk';
 import { withStaticFrameworkBuildType } from './withIosIdnowVideoIdentSdk';
-
-const pak = require('../../../package.json');
 
 const withIdnowVideoIdentSdk: ConfigPlugin = (expoConfig: ExpoConfig) => {
     expoConfig = withStaticFrameworkBuildType(expoConfig);
@@ -13,5 +12,4 @@ const withIdnowVideoIdentSdk: ConfigPlugin = (expoConfig: ExpoConfig) => {
     return expoConfig;
 };
 
-const typedPak: { name: string; version: string } = pak;
 export default createRunOncePlugin(withIdnowVideoIdentSdk, typedPak.name, typedPak.version);
--- src/expo/withIosIdnowVideoIdentSdk.ts
+++ src/expo/withIosIdnowVideoIdentSdk.ts
@@ -4,54 +4,63 @@ import type { ExpoConfig } from '@expo/config-types';
 import { readFileSync, writeFileSync } from 'fs';
 import { join } from 'path';
 
+import { typedPak } from './typedPak';
+
 const bufferEncoding: BufferEncoding = 'utf8';
 
 const editPodfile = (config: ExportedConfigWithProps<unknown>, action: (podfile: string) => string): void => {
     const podfilePath = join(config.modRequest.platformProjectRoot, 'Podfile');
     try {
-        const podfile = action(readFileSync(podfilePath, bufferEncoding));
-        writeFileSync(podfilePath, podfile, bufferEncoding);
+        const newPodfile = action(readFileSync(podfilePath, bufferEncoding));
+        writeFileSync(podfilePath, newPodfile, bufferEncoding);
     } catch (error) {
-        WarningAggregator.addWarningIOS('idnow', `Couldn't modified Podfile - ${error}.`);
+        WarningAggregator.addWarningIOS(typedPak.name, `Couldn't modified Podfile - ${error}.`);
     }
 };
 
-const addLines = (content: string, find: string, offset: number, toAdd: Array<string>): string => {
+const addLines = (content: string, find: string, offset: number, toAdd: string): string => {
     const lines = content.split('\n');
-
-    let lineIndex = lines.findIndex((line: string) => line.match(find));
-
-    for (const newLine of toAdd) {
-        lines.splice(lineIndex + offset, 0, newLine);
-        lineIndex += 1;
-    }
+    const lineIndex = lines.findIndex((line: string) => line.match(find));
+    lines.splice(lineIndex + offset, 0, toAdd);
 
     return lines.join('\n');
 };
+
+const buildTypeModification =
+    '  $static_library = %w[IDnowSDK Masonry SocketRocket libPhoneNumber-iOS FLAnimatedImage AFNetworking]\n' +
+    '\n' +
+    '  pre_install do |installer|\n' +
+    '    Pod::Installer::Xcode::TargetValidator.send(:define_method, :verify_no_static_framework_transitive_dependencies) {}\n' +
+    '    installer.pod_targets.each do |pod|\n' +
+    '      bt = pod.send(:build_type)\n' +
+    '      if $static_library.include?(pod.name)\n' +
+    "        puts 'Overriding the build_type to static_framework from static_library for #{pod.name}'\n" +
+    '        def pod.build_type\n' +
+    '          Pod::BuildType.static_framework\n' +
+    '        end\n' +
+    '      end\n' +
+    '    end\n' +
+    '    installer.pod_targets.each do |pod|\n' +
+    '      bt = pod.send(:build_type)\n' +
+    "      puts '#{pod.name} (#{bt})'\n" +
+    "      puts '  linkage: #{bt.send(:linkage)} packaging: #{bt.send(:packaging)}'\n" +
+    '    end\n' +
+    '  end\n';
+const appleSiliconFix =
+    '    # https://github.com/expo/expo/issues/15800\n' +
+    '    installer.pods_project.targets.each do |target|\n' +
+    '      target.build_configurations.each do |config|\n' +
+    "        config.build_settings['ONLY_ACTIVE_ARCH'] = 'NO'\n" +
+    '      end\n' +
+    '    end\n';
 
 export const withStaticFrameworkBuildType = (config: ExpoConfig): ExpoConfig =>
     withDangerousMod(config, [
         'ios',
         (withDangerousModConfig): ExportedConfigWithProps => {
             editPodfile(withDangerousModConfig, (podfile) => {
-                podfile = addLines(podfile, ':deterministic_uuids => false', 1, [
-                    '',
-                    "plugin 'cocoapods-user-defined-build-types'",
-                    'enable_user_defined_build_types!',
-                ]);
-                podfile = addLines(podfile, 'flags = get_default_flags()', 1, [
-                    '',
-                    "  pod 'IDnowSDK', '5.3.0', build_type: :static_framework",
-                ]);
-                // https://github.com/expo/expo/issues/15800
-                podfile = addLines(podfile, 'react_native_post_install', 2, [
-                    '',
-                    '    installer.pods_project.targets.each do |target|',
-                    '      target.build_configurations.each do |config|',
-                    '        config.build_settings["ONLY_ACTIVE_ARCH"] = "NO"',
-                    '      end',
-                    '    end',
-                ]);
+                podfile = addLines(podfile, 'flags = get_default_flags()', 10, buildTypeModification);
+                podfile = addLines(podfile, 'react_native_post_install', 3, appleSiliconFix);
                 return podfile;
             });
             return withDangerousModConfig;
