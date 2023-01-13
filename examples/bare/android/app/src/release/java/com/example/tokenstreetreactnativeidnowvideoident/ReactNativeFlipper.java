ab46c76beb18fa041e011b33e1a2f84772d7cd7d
Daniel Reichhart <daniel.reichhart@gmx.net>
2023-01-13 11:39:34 +0100

    draft

--- examples/bare/android/app/src/release/java/com/example/tokenstreetreactnativeidnowvideoident/ReactNativeFlipper.java
+++ examples/bare/android/app/src/release/java/com/example/tokenstreetreactnativeidnowvideoident/ReactNativeFlipper.java
@@ -14,7 +14,8 @@ import com.facebook.react.ReactInstanceManager;
  * flavor of it so it's empty as we don't want to load Flipper.
  */
 public class ReactNativeFlipper {
-  public static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
-    // Do nothing as we don't want to initialize Flipper on Release.
-  }
+
+    public static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
+        // Do nothing as we don't want to initialize Flipper on Release.
+    }
 }
