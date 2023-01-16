b3d14bcf641432bcb8e6e42be18aa2f092279e81
Daniel Reichhart <daniel.reichhart@gmx.net>
2023-01-16 10:41:35 +0100

    chore: package

--- android/src/main/java/com/tokenstreet/reactnativeidnowvideoident/ReactNativeIdnowVideoidentPackage.kt
+++ android/src/main/java/com/tokenstreet/reactnativeidnowvideoident/ReactNativeIdnowVideoidentPackage.kt
@@ -1,3 +1,10 @@
+/*
+ * Copyright © 2022 tokenstreet GmbH.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 package com.tokenstreet.reactnativeidnowvideoident
 
 import com.facebook.react.ReactPackage
