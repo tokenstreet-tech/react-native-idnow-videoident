a8f5767862cf1c85dcad5a7d08beaf8f56f8be05
Daniel Reichhart <daniel.reichhart@gmx.net>
2024-01-05 14:28:57 +0100

    chore: draft

--- examples/bare/android/app/src/main/java/com/example/tokenstreetreactnativeidnowvideoident/MainApplication.kt
+++ examples/bare/android/app/src/main/java/com/example/tokenstreetreactnativeidnowvideoident/MainApplication.kt
@@ -21,7 +21,7 @@ class MainApplication : Application(), ReactApplication {
           val packages = PackageList(this).packages
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          packages.add( ReactNativeIdnowVideoidentPackage())
+          packages.add(ReactNativeIdnowVideoidentPackage())
           return packages;
         }
 
