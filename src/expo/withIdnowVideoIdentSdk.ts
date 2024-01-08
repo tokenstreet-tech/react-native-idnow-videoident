9d5f9932fcbf81b342d6658d7150d1b8d68a9bc5
Daniel Reichhart <daniel.reichhart@gmx.net>
2024-01-08 16:54:37 +0100

    chore: draft

--- src/expo/withIdnowVideoIdentSdk.ts
+++ src/expo/withIdnowVideoIdentSdk.ts
@@ -4,10 +4,8 @@ import { createRunOncePlugin } from '@expo/config-plugins';
 import type { IConfigPluginProps } from './model/IConfigPluginProps';
 import { typedPak } from './util/typedPak';
 import { withIdnowRepositories } from './withAndroidIdnowVideoIdentSdk';
-import { withStaticFrameworkBuildType } from './withIosIdnowVideoIdentSdk';
 
 const withIdnowVideoIdentSdk: ConfigPlugin<IConfigPluginProps> = (expoConfig, props) => {
-    expoConfig = withStaticFrameworkBuildType(expoConfig, props);
     expoConfig = withIdnowRepositories(expoConfig, props);
     return expoConfig;
 };
