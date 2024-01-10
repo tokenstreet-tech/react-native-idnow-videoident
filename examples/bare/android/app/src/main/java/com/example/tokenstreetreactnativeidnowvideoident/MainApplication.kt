a4e68a3d88f3f3eaf0727cb1e7a884e396ffe561
Daniel Reichhart <34721312+reichhartd@users.noreply.github.com>
2024-01-10 13:02:11 +0100

    chore: Update deps (#1352)

--- examples/bare/android/app/src/main/java/com/example/tokenstreetreactnativeidnowvideoident/MainApplication.kt
+++ examples/bare/android/app/src/main/java/com/example/tokenstreetreactnativeidnowvideoident/MainApplication.kt
@@ -17,13 +17,12 @@ class MainApplication : Application(), ReactApplication {
 
     override val reactNativeHost: ReactNativeHost =
         object : DefaultReactNativeHost(this) {
-            override fun getPackages(): List<ReactPackage> {
-                val packages = PackageList(this).packages
-                // Packages that cannot be autolinked yet can be added manually here, for example:
-                // packages.add(new MyReactNativePackage());
-                packages.add(ReactNativeIdnowVideoidentPackage())
-                return packages
-            }
+            override fun getPackages(): List<ReactPackage> =
+                PackageList(this).packages.apply {
+                    // Packages that cannot be autolinked yet can be added manually here, for example:
+                    // add(MyReactNativePackage())
+                    packages.add(ReactNativeIdnowVideoidentPackage())
+                }
 
             override fun getJSMainModuleName(): String = "index"
 
